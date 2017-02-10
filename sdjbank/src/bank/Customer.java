15c52d2a1892e67bf752bf05662cb006bdd54cb2
Unknown <andrey_europe_95hd@yahoo.com>
2017-02-10 11:28:31 +0100

    customer class progress, account class

--- sdjbank/src/bank/Customer.java
+++ sdjbank/src/bank/Customer.java
@@ -1,5 +1,16 @@
 package bank;
 
-public class Customer {
+import java.util.ArrayList;
 
+public class Customer {
+	private String name;
+	private int cpr;
+	private String address;
+	private ArrayList<Account> accounts;
+	
+	public Customer(String name, int cpr, String address) {
+		this.name = name;
+		this.cpr = cpr;
+		this.address = address;
+	}
 }
